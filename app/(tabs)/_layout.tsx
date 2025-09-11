58e0fa4a5076f4c4396b3dc05643847f70d69b96
hyy <hyy@hyydeMacBook-Air.local>
2025-09-11 14:17:08 +0800

    增加bar样式

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,6 +9,10 @@ const Logo = () => {
   );
 };
 
+const TabButton = (props) => {
+  const { name, ...rest } = props;
+  return <SimpleLineIcons size={20} name={name} {...rest} />;
+};
 const HeaderButton = (props) => {
   const { name, ...rest } = props;
   return (
@@ -33,10 +37,31 @@ const TabLayout = () => {
             <HeaderButton name="options" href="/video" />
           </>
         ),
+        tabBarActiveTintColor: "#1f99b0",
+        // 去除安卓手机切换tab的波纹样式
+        tabBarButton: (props) => (
+          <TouchableOpacity
+            {...props}
+            activeOpacity={1}
+            style={[props.style, { backgroundColor: "transparent" }]}
+          />
+        ),
       }}
     >
-      <Tabs.Screen name="index" options={{ title: "配置" }}></Tabs.Screen>
-      <Tabs.Screen name="video" options={{ title: "视频" }}></Tabs.Screen>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "配置",
+          tabBarIcon: ({ color }) => <TabButton name="compass" color={color} />,
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="video"
+        options={{
+          title: "视频",
+          tabBarIcon: ({ color }) => <TabButton name="user" color={color} />,
+        }}
+      ></Tabs.Screen>
     </Tabs>
   );
 };
